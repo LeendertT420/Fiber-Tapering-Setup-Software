fa09b98f24f3bb6ec8169ce7950749401edc499f
Timmerman <Timmerman@Physics.LeidenUniv.nl>
2023-09-15 17:43:46 +0200

    controller and axis classes with first test + labview code Kellan

--- fiber_tapering_test.py
+++ fiber_tapering_test.py
@@ -19,31 +19,103 @@ class Controller_Axis():
         self.des_pos = None
         self.set_vel = None
 
+        self.update_axis()
+
+
     def get_pos(self) -> float:
+        """
+        Queries the current position of the axis relative to the home postion. 
+
+        Returns:
+            - pos (float) : current position in mm
+        """
         command = str(self.axis_nr) + 'TP'
         pos = self.controller.query(command)
         return pos
     
+
     def get_vel(self) -> float:
+        """
+        Queries the current velocity of the axis. 
+
+        Returns:
+            - vel (float) : current velocity in mm/s
+        """
         command = str(self.axis_nr) + 'TV'
         vel = self.controller.query(command)
         return vel
     
+
     def get_des_pos(self) -> float:
+        """
+        Queries the destination position of the axis relative to the home 
+        position. Only relevant if the axis is moving. It gives the position 
+        the axis is moving to. 
+
+        Returns:
+            - des_pos (float) : destination position in mm
+        """
         command = str(self.axis_nr) + 'DP'
         des_pos = self.controller.query(command)
         return des_pos
     
+
     def get_set_vel(self) -> float:
+        """
+        Queries the set velocity of the axis. This is the velocity of the axis
+        when moving to the destination position. This can be different from 
+        what self.get_vel() returns when the motor is homing (homing velocity
+        can be different from set velocity), in between movements (stationary)
+        or starting up a movement. 
+        
+        Returns:
+            - set_vel (float) : set velocity in mm/s
+        """
         command = str(self.axis_nr) + 'DV'
         set_vel = self.controller.query(command)
         return set_vel
     
-    def update_status(self) -> None:
+
+    def update_axis(self) -> None:
+        """
+        Update position and velocity metrics by calling the get functions.
+        """
         self.pos = self.get_pos()
         self.vel = self.get_vel()
         self.des_pos = self.get_des_pos()
         self.set_vel = self.get_set_vel()
+
+
+    def set_abs_pos(self, abs_pos : float) -> None:
+        """
+        Set the absolute destination position of the axis relative to the home
+        position. 
+
+        params:
+            - abs_pos (float) : absolute position to move to in mm
+        """
+        if abs_pos < self.home_pos or abs_pos > 25 + self.home_pos:
+            raise Exception("Position is out of range")
+        
+        command = str(self.axis_nr) + 'PA' + str(abs_pos)
+        self.controller.write(command)
+
+
+    def set_rel_pos(self, rel_pos : float) -> None:
+        """
+        Set the destination position of the axis relative to its current position.  
+
+        params:
+            - rel_pos (float) : relative position to move to in mm
+        """
+        abs_pos = rel_pos + self.get_pos()
+
+        if abs_pos < self.home_pos or abs_pos > 25 + self.home_pos:
+            raise Exception("Position is out of range")
+        
+        command = str(self.axis_nr) + 'PR' + str(rel_pos)
+        self.controller.write(command)
+
     
 
 
@@ -53,9 +125,11 @@ class Motion_Controller():
         
         self.controller = self.get_controller()
 
-        self.ax1 = Controller_Axis(1, home_pos = home_positions[0])
-        self.ax2 = Controller_Axis(2, home_pos = home_positions[1])
-        self.ax3 = Controller_Axis(3, home_pos = home_positions[2])
+        ax1 = Controller_Axis(1, self.controller, home_pos = home_positions[0])
+        ax2 = Controller_Axis(2, self.controller, home_pos = home_positions[1])
+        ax3 = Controller_Axis(3, self.controller, home_pos = home_positions[2])
+
+        self.axes = [ax1, ax2, ax3]
 
         self.print_status()
 
@@ -71,51 +145,38 @@ class Motion_Controller():
                                 data_bits = 8)
         return controller
 
+
     def update_status(self) -> None:
 
-        positions = [None] * 3
-
-        for ax in range(1, 4):
-            positions[ax - 1] = self.get_pos(ax)
+        for ax in self.axes:
+            ax.update_axis()
         
-        self.pos_ax1, self.pos_ax2, self.pos_ax3 = positions 
-            
-
-    def get_pos(self, ax : int) -> float:
-        command = str(ax) + 'TP'
-        pos = self.controller.query(command)
-        return pos
-    
-    def get_vel(self, ax : int) -> float:
-        command = str(ax) + 'TV'
-        vel = self.controller.query(command)
-        return vel
-    
-    def get_des_pos(self, ax : int) -> float:
-        command = str(ax) + 'DP'
-        des_pos = self.controller.query(command)
-        return des_pos
-    
-    def get_set_vel(self, ax : int) -> float:
-        command = str(ax) + 'DV'
-        set_vel = self.controller.query(command)
-        return set_vel
-    
-    def set_abs_pos(self, ax : int, pos : int) -> None:
-        command = 
     
 
 
     def print_status(self) -> None:
-        self.update_status()
 
-        print('current positions (mm)')
-        print(f'axis 1: {self.pos_ax1}')
-        print(f'axis 2: {self.pos_ax2}')
-        print(f'axis 3: {self.pos_ax3}')
+        for ax in self.axes:
+            ax.update_axis()
+            print(f'Axis {ax.axis_nr}')
+            print(f'pos: {ax.pos} mm')
+            print(f'vel: {ax.vel} mm/s')
+            print(f'des_pos: {ax.des_pos} mm')
+            print(f'set_vel: {ax.set_vel} mm/s')
+            print()
+        print()
 
 
-newport_controller = Motion_Controller()
+
+
+
+if __name__ is "__main__":
+    controller = Motion_Controller()
+    controller.axes[0].get_pos()
+
+
+
+
             
 
 
